385d86de1273352f1f11bfa83ec309a5cf354c34
Supachai Mangkud <supachaimangkud@gmail.com>
2023-10-20 09:32:28 +0100

    basic player movement
    no jump atm because we need to build terrain first

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSentivity = 100f;
+
+    //a reference from the main camera to our entire first person player object so that we can rotate around
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Time.deltatime is the amount of time that has gone by since the last time the update function was called
+        float mouseX = Input.GetAxis("Mouse X") * mouseSentivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSentivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
