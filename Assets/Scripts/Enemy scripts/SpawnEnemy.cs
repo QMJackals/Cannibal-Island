43d63c1b90a78d988f9c14ad7b7a795b01ed714a
Yesh Chandiramani <yesh0907@hotmail.com>
2023-10-24 23:54:22 +0100

    spawn enemy close to player and on land only

--- Assets/Scripts/Enemy scripts/SpawnEnemy.cs
+++ Assets/Scripts/Enemy scripts/SpawnEnemy.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    public Transform player;
     public GameObject EnemyPrefab;
     public int enemyCount = 5;
+
+    private int randomRange = 50;
+
+    private float minX = 20;
+    private float minZ = 20;
+    private float maxX = 997;
+    private float maxZ = 997;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +21,23 @@ public class SpawnEnemy : MonoBehaviour
         Spawn();
     }
 
+    // Spawn enemies around the player's location
     private void Spawn()
     {
         // Code was inspired by this tutorial: https://subscription.packtpub.com/book/game-development/9781783553655/1/ch01lvl1sec13/creating-enemies
         for (int i = 0; i < enemyCount; i++)
         {
-            float randomX = Random.Range(-100, 101);
-            float randomZ = Random.Range(-100, 101);
+            float playerX = player.transform.position.x;
+            float playerY = player.transform.position.y;
+            float playerZ = player.transform.position.z;
 
-            Instantiate(EnemyPrefab, new Vector3(randomX, 2, randomZ), Quaternion.identity);
+            float randomX = Random.Range(-randomRange + playerX, randomRange + 1 + playerX);
+            float randomZ = Random.Range(-randomRange + playerZ, randomRange + 1 + playerZ);
+
+            randomX = Mathf.Clamp(randomX, minX, maxX);
+            randomZ = Mathf.Clamp(randomZ, minZ, maxZ);
+
+            Instantiate(EnemyPrefab, new Vector3(randomX, playerY, randomZ), Quaternion.identity);
         }
     }
 }
