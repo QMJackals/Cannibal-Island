94afd484209a51871bb16b51df9b5ecb6a2ea903
MohammedAbubakar <Mohammed.Abubakar_03@Outlook.com>
2023-10-26 15:36:56 +0100

    Comment code in StabAnimation.cs

--- Assets/Scripts/Player scripts/StabAnimation.cs
+++ Assets/Scripts/Player scripts/StabAnimation.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is inspired by the following tutorial: https://www.youtube.com/watch?v=gQI2ZjCK2RA&t
+// It triggers the simple knife attack animation upon left mouse click
 public class StabAnimation : MonoBehaviour
 {
     Animator anim;
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>(); // Gets the animator component
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            anim.SetTrigger("StabTrigger");
+            anim.SetTrigger("StabTrigger"); // Upon left mouse click, trigger the stab animation
         }
     }
 }
