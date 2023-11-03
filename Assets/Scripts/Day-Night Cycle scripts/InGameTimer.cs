8adfb2386156ee0537eb0032fdd114f973ae86b2
Yesh Chandiramani <yesh0907@hotmail.com>
2023-11-03 10:42:40 +0000

    spawn enemies based on day-night cycle

--- Assets/Scripts/Day-Night Cycle scripts/InGameTimer.cs
+++ Assets/Scripts/Day-Night Cycle scripts/InGameTimer.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+public enum TimeCycle
+{
+    DAY,
+    NIGHT,
+};
+
 // This script calculates whether its day or night based on the elapsed timer
 public class InGameTimer : MonoBehaviour
 {
@@ -10,17 +16,21 @@ public class InGameTimer : MonoBehaviour
     [SerializeField] private TMP_Text inGameTimer_Text;
     [SerializeField] private bool isDone;
     public int count; // Needed to count how many days have passed
+    public TimeCycle timeCycle;
+
     void Start()
     {
         inGameTimer_Text = GetComponent<TextMeshProUGUI>(); // Accesses the text in the UI
         count = -1; // Resets the counter
         isDone = false;
+        timeCycle = TimeCycle.DAY;
     }
     void Update()
     {
         float eTime = Mathf.Round(et.elapsedTime); // Rounds the elapsed time to the nearest second
         if (eTime % 10 == 0) // Every 10 seconds, display the game time as "Day"
         {
+            timeCycle = TimeCycle.DAY;
             inGameTimer_Text.text = "In Game Time: Day";
             while (!isDone) // After each day has begun, increments the count (only once each day, then stops incrementing)
             {
@@ -30,6 +40,7 @@ public class InGameTimer : MonoBehaviour
         }
         if (eTime % 5 == 0 && eTime % 10 != 0) // Otherwise, display the game time as "Night"
         {
+            timeCycle = TimeCycle.NIGHT;
             inGameTimer_Text.text = "In Game Time: Night";
             isDone = false; // Resets isDone variable, so next day can trigger another count increment
         }
--- Assets/Scripts/Enemy scripts/SpawnEnemy.cs
+++ Assets/Scripts/Enemy scripts/SpawnEnemy.cs
@@ -7,8 +7,10 @@ public class SpawnEnemy : MonoBehaviour
 {
     public Transform player;
     public GameObject EnemyPrefab;
-    public int enemyCount = 5;
+    public int maxEnemyCount = 5;
+    public int numOfWaves = 2;
     public TextMeshProUGUI winText;
+    public InGameTimer gameTimer;
 
     private int randomRange = 50;
 
@@ -17,22 +19,34 @@ public class SpawnEnemy : MonoBehaviour
     private float maxX = 997;
     private float maxZ = 997;
 
+    int enemyCount = 0;
+    int waves = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         winText.gameObject.SetActive(false);
-        Spawn();
+    }
+
+    private void Update()
+    {
+        if (gameTimer.timeCycle == TimeCycle.NIGHT && waves < numOfWaves && (waves == 0 || enemyCount <= 3))
+        {
+            Spawn();
+        }
     }
 
     // Spawn enemies around the player's location
     private void Spawn()
     {
         // Code was inspired by this tutorial: https://subscription.packtpub.com/book/game-development/9781783553655/1/ch01lvl1sec13/creating-enemies
-        for (int i = 0; i < enemyCount; i++)
+        for (int i = 0; i < maxEnemyCount; i++)
         {
             float playerX = player.transform.position.x;
             float playerY = player.transform.position.y;
             float playerZ = player.transform.position.z;
+
+            Debug.Log(player.transform.position);
 
             float randomX = Random.Range(-randomRange + playerX, randomRange + 1 + playerX);
             float randomZ = Random.Range(-randomRange + playerZ, randomRange + 1 + playerZ);
@@ -42,12 +56,15 @@ public class SpawnEnemy : MonoBehaviour
 
             Instantiate(EnemyPrefab, new Vector3(randomX, playerY, randomZ), Quaternion.identity);
         }
+        enemyCount += maxEnemyCount;
+        waves++;
     }
 
     public void UpdateEnemyCountBy(int amount)
     {
+        // This function is used to decrement the enemy count when the player kills enemies
         enemyCount += amount;
-        if (enemyCount <= 0)
+        if (enemyCount <= 0 && waves >= numOfWaves)
         {
             winText.gameObject.SetActive(true);
         }
